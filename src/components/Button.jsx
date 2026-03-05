6009c48299f94a5d52bbd27b68f55af32a5bbe5b
Sheeba193 <bethkerubo193@gmail.com>
2026-03-05 14:21:41 +0300

    button design and mapping items to hero section

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+const Button = ({ label, iconURL }) => {
+  return (
+    <button className="flex items-center justify-center 
+    gap-2 bg-coral-red text-lg px-7 py-4 border font-montserrat
+    leading-none text-white border-coral-red rounded-full">
+        {label}
+        <img src={iconURL} alt="shop now"
+        className="ml-2 rounded-full h-5 w-5" />
+
+    </button>
+  )
+}
+
+export default Button
