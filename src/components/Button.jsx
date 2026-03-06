bb075b06d31e0d7cc9008445439fe5701bae28a5
Sheeba193 <bethkerubo193@gmail.com>
2026-03-07 01:07:39 +0300

    implement interactive shoe selection in Hero section and minor UI fixes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className="flex items-center justify-center 
     gap-2 bg-coral-red text-lg px-7 py-4 border font-montserrat
-    leading-none text-white border-coral-red rounded-full">
+    leading-none text-white border-coral-red rounded-full w-fit">
         {label}
         <img src={iconURL} alt="shop now"
         className="ml-2 rounded-full h-5 w-5" />
