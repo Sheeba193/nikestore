82c8742281dc82fb10fc924be7da36a75d507539
Sheeba193 <bethkerubo193@gmail.com>
2026-03-08 04:17:02 +0300

    implement Popular Products section and card component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -18,8 +18,7 @@ const Hero = () => {
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
             <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span>
-          Shoes
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p className="font-montserrat justify-start items-start flex-wrap w-full mt-20 gap-16">Discover the latest Nike shoes designed forncomfort and performance.</p>
 
