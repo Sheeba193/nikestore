abf256bac5d7917667ba2065b0e43b4318024ade
Sheeba193 <bethkerubo193@gmail.com>
2026-03-12 01:33:16 +0300

    add ReviewCard component and implement CustomerReviews section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,27 @@
+import { reviews } from "../constants"
+import ReviewCard from "../components/ReviewCard"
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">What Our 
+      <span className="text-coral-red">Customers </span>
+      Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear from our satisfied customers who have experienced the quality and style of our products. We take pride in delivering exceptional service and ensuring that our customers are happy with their purchases. Read their reviews and see why they love shopping with us!
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard key={review.customerName} 
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
